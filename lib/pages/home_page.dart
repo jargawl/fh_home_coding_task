48d9e5ca66bae7a33af7dd90880155f9a4309192
Jaroslaw Gawlik <jargawl@gmail.com>
2022-07-15 00:20:19 +0200

    Implement Water Leak Detector section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:fh_home_coding_task/pages/section/board_member.dart';
 import 'package:fh_home_coding_task/pages/section/smoke_detector.dart';
 import 'package:fh_home_coding_task/pages/section/up_secion.dart';
+import 'package:fh_home_coding_task/pages/section/water_leak_detector.dart';
 
 import 'package:flutter/material.dart';
 
@@ -10,15 +11,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: Column(
-          children: const [
-            SizedBox(height: 10),
-            UpSecion(),
-            BoardMembers(),
-            SmokeDetector(),
-          ],
-        ),
+      body: Column(
+        children: const [
+          SizedBox(height: 10),
+          UpSecion(),
+          BoardMembers(),
+          SmokeDetector(),
+          WaterLeakDetector(),
+        ],
       ),
     );
   }
---
+++ lib/pages/section/water_leak_detector.dart
@@ -0,0 +1,92 @@
+import 'package:fh_home_coding_task/widgets/buttons/dark_button.dart';
+import 'package:fh_home_coding_task/widgets/buttons/icon_button.dart';
+import 'package:flutter/material.dart';
+
+class WaterLeakDetector extends StatelessWidget {
+  const WaterLeakDetector({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Expanded(
+        child: Container(
+          width: 1400,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.lightBlue),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 5),
+                Row(
+                  children: const [
+                    SizedBox(width: 20),
+                    Icon(Icons.question_mark),
+                    Text('Water leak detector'),
+                    SizedBox(width: 180),
+                    Icon(Icons.check_outlined),
+                    Text('Online'),
+                    SizedBox(width: 250),
+                    Icon(Icons.check_outlined),
+                    Text('Secured'),
+                    SizedBox(width: 200),
+                    Icon(Icons.check_outlined),
+                    Text('Configured'),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: const [
+                    SizedBox(width: 25),
+                    Text(
+                      'ADDRESS',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 265),
+                    Text('2_0'),
+                    Icon(Icons.question_mark),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: const [
+                    SizedBox(width: 25),
+                    Text(
+                      'DEVICE ID',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 265),
+                    Text('1'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: const [
+                    SizedBox(width: 25),
+                    DarkButton(text: 'EDIT'),
+                    SizedBox(width: 15),
+                    DarkButton(text: 'UNPAIR'),
+                    SizedBox(width: 15),
+                    DarkButton(text: 'IDENTIFY'),
+                    SizedBox(width: 15),
+                    DarkButton(text: 'PING'),
+                    SizedBox(width: 750),
+                    CustomIconButton(text: 'TIMELINE'),
+                    SizedBox(width: 15),
+                    CustomIconButton(text: '8 NOTES'),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
